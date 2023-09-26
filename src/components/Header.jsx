38762808992331990ed965a5675a603d03dd9016
aun1x <tirthankar.nath2@gmail.com>
2023-09-26 21:51:38 +0530

    dark and light mode implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
 import { Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
 
 function Header() {
+	const { colorMode, toggleColorMode } = useColorMode();
   return (
+    <>
     <div>Headers</div>
+    <Flex justifyContent={"center"} mt={6} mb='12'>
+			<Image
+				cursor={"pointer"}
+				alt='logo'
+				w={6}
+				src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+				onClick={toggleColorMode}
+			/>
+
+    </Flex>
+    </>
   )
 }
 
