ec0373009f8b12f2aea1cf9f698a36ea107c5188
aun1x <tirthankar.nath2@gmail.com>
2023-09-28 14:17:29 +0530

    the initial setup of routes is done, we are now able to send init get requests

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import  express  from "express";
 import dotenv from 'dotenv'
 import connectDB from './db/connectDB.js'
+import cookieParser from 'cookie-parser'
+
+import userRoutes from './routes/userRoutes.js'
+
+
 
 dotenv.config();
 connectDB();
@@ -8,5 +13,13 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //parse JSON data in the req.body
+app.use(express.urlencoded({extended: true})); //parse form data in the req.body, allows nested objects 
+app.use(cookieParser());
+
+
+//routes
+app.use("/api/users",userRoutes)
 
 app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT} `))
